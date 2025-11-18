d45d4393dd84cf956dc7f3aeccc5cb3a64b4d649
FranF3 <franco.amoroso@fathom3dev.com>
2025-11-18 20:51:06 +0100

    refactor(root): layout

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,32 +1,33 @@
+import { Readex_Pro as FontSans } from "next/font/google";
 import React, { type ReactNode } from 'react'
 
 import { Layout } from 'components/Layout'
+import StyledComponentsRegistry from 'lib/StyledComponentsRegistry'
 
 import { ApolloWrapper } from './ApolloWrapper'
 
 import type { Metadata } from 'next'
 import 'assets/globals/styles.css'
 
-const IS_DEV = process.env.NODE_ENV !== 'production'
-
 export const metadata: Metadata = {
-  title: `UI challenge ${IS_DEV ? ' (dev)' : ' (prod)'}`,
+  title: "UI challenge Franco Amoroso",
   description: 'UI challenge application',
 }
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export default function RootLayout({ children }: { children: ReactNode }): ReactNode {
   return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <ApolloWrapper>
-          <Layout>{children}</Layout>
-        </ApolloWrapper>
+    <html lang="en" suppressHydrationWarning>
+      <body className={fontSans.variable}>
+        <StyledComponentsRegistry>
+          <ApolloWrapper>
+            <Layout>{children}</Layout>
+          </ApolloWrapper>
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
