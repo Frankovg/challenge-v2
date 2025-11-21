b45f20bf95aaba80b691ff934d435d37281d65fe
FranF3 <franco.amoroso@fathom3dev.com>
2025-11-21 20:54:48 +0100

    equal padding

--- apps/frontend/src/components/Packing/PackedSection/PackedSection.styles.ts
+++ apps/frontend/src/components/Packing/PackedSection/PackedSection.styles.ts
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 export const PackedSectionContainer = styled.section`
   width: 70%;
-  padding: 20px;
+  padding: 1.2rem;
   display: flex;
   flex-direction: column;
   border-left: 1px solid var(--border-primary);
--- apps/frontend/src/components/Packing/UnpackedSection/UnpackedSection.styles.ts
+++ apps/frontend/src/components/Packing/UnpackedSection/UnpackedSection.styles.ts
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 export const UnpackedSectionContainer = styled.section`
   width: 30%;
-  padding: 1.5rem;
+  padding: 1.2rem;
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
